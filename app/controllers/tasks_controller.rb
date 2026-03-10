f519f83b4e73eeccf1c6205d191329835b7ac3ae
Yogita Maruti <157566073+y-m-codes@users.noreply.github.com>
2026-03-10 11:19:19 +1100

    task manager, routes and views creation

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  def index
+    # lists all tasks
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    # renders the view for user to input new task details in memory,
+    # does not save to DB (not persisted)
+    @task = Task.new
+  end
+
+  def create
+    # makes new task, saves to DB (persisted)
+    @task = Task.new(task_params)
+    @task.save
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # renders the view to get user input to modify an existing record,
+    # does not save the input
+  end
+
+  def update
+    # saves the user changes from "edit" to the DB, updates the record
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # deletes a selected record
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "tasks#index"
+
+  resources :tasks
+
+  get 'tasks', to: "tasks#index"
+  get 'tasks/find', to:
+  get 'tasks/:id', to: "tasks#show"
+  get 'tasks/new', to: "tasks#new"
+  post 'tasks', to: "tasks#create"
+  patch 'tasks/:id', to: "tasks#update"
+  delete 'tasks/:id', to: "tasks#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
 end
